a6485f50fc924eaf631b754336d6c0eff672c609
bostjangec <bostogec@gmail.com>
2022-11-08 09:58:09 +0100

    ph_metric downsample

---
+++ ProGED/examples/DS2022/special-issue/TDA-Python-Workshop-JMM21-main/MLJ_add_noise_to_data.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_noisy_data(data, save_plot=True, plot_path="", plot_name=""):
+
+    t = data[:, 0]
+    amplitudes = data[:, 1:]
+    n_vars = np.size(amplitudes, 1)
+
+    # plot the time series data
+    fig1 = plt.figure()
+    for ip in range(n_vars):
+        plt.plot(t, amplitudes[:, ip])
+    plt.title('Signal')
+    plt.ylabel('Voltage (V)')
+
+    fig2 = plt.figure()
+    for ip in range(n_vars):
+        plt.plot(t[1000:2000], amplitudes[1000:2000, ip])
+    plt.title('Signal')
+    plt.ylabel('Voltage (V)')
+
+    # plot the phase plot
+    fig3 = plt.figure()
+    if n_vars == 2:
+        plt.plot(amplitudes[:, 0], amplitudes[:, 1])
+    elif n_vars == 3:
+        ax = plt.axes(projection='3d')
+        ax.plot3D(amplitudes[:, 0], amplitudes[:, 1], amplitudes[:, 2])
+    elif n_vars == 4:
+        plt.subplot(1, 2, 1)
+        plt.plot(amplitudes[:, 0], amplitudes[:, 1])
+        plt.subplot(1, 2, 2)
+        plt.plot(amplitudes[:, 2], amplitudes[:, 3])
+    plt.title('phase plot')
+
+    if save_plot:
+        fig1.savefig(plot_path + plot_name.format("truedata_timeplot", "png"), dpi=300)
+        fig2.savefig(plot_path + plot_name.format("truedata_timeplot_part", "png"), dpi=300)
+        fig3.savefig(plot_path + plot_name.format("truedata_phaseplot", "png"), dpi=300)
+
+    plt.close('all')
+
+def add_noise_to_data(data, target_noise_db=None, target_snr_db=None):
+
+    # seed and general vars definition
+    mean_noise = 0
+    n_vars = np.size(data, 1) - 1
+
+    # set the output
+    data_out = np.empty_like(data)
+    data_out[:, 0] = data[:, 0]
+
+    # calculate signal power and power in db
+    amplitudes = data[:, 1:]
+    powers = amplitudes ** 2
+
+    # Calculate signal avg power and convert to dB
+    sig_avg_power = np.mean(powers, 0)
+    sig_avg_db = 10 * np.log10(sig_avg_power)
+
+    # Add noise based on either desired snr or noise
+    if target_snr_db is not None:
+        noise_avg_db = sig_avg_db - target_snr_db
+        noise_avg_power = 10 ** (noise_avg_db / 10)
+        for ivar in range(n_vars):
+            noise = np.random.normal(mean_noise, np.sqrt(noise_avg_power)[ivar], len(powers))
+            data_out[:, ivar+1] = amplitudes[:, ivar] + noise
+
+    elif target_noise_db is not None:
+        noise_avg_power = 10 ** (target_noise_db / 10)
+        for ivar in range(n_vars):
+            noise = np.random.normal(mean_noise, np.sqrt(noise_avg_power), len(powers))
+            data_out[:, ivar+1] = amplitudes[:, ivar] + noise
+
+    else:
+        raise Exception('Either target_noise_db or target_snr_sb have to be defined.')
+
+    return data_out
---
+++ ProGED/examples/DS2022/special-issue/TDA-Python-Workshop-JMM21-main/README.md
@@ -0,0 +1,23 @@
+# TDA-Python-Workshop-JMM21
+
+This repository includes the jupyter notebooks for the 2021 Workshop ["Mathematical and Computational Methods for Complex Social Systems"](https://www.google.com/url?q=https://meetings.ams.org/math/sc2021/meetingapp.cgi) to be held at the virtual JMM 2021, led by [Elizabeth Munch](http://elizabethmunch.com/). This introduction is tailored for a network science audience, so the focus is on an relevant tools when given a network input.
+
+The goals of this 1-hour introduction are:
+
+- Give a brief overview of available packages
+- Provide pipelines for computing persistent homology for input data such as a discrete metric space and a weighted graph.  
+- Show some basic ways to interface with machine learning toolkits
+
+
+
+# Binder link
+
+To run the notebook online, no installs necessary, click here: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/lizliz/TDA-Python-Workshop-JMM21/HEAD?filepath=PythonTDA_Intro.ipynb)
+
+# Links to video
+
+This was presented at the 2021 JMM.  The video of the event is hosted [here](https://zerodivzero.com/short_course/aaac8c66007a4d23a7aa14857a3b778c/title/cffb85f269864df08b61382c00c77c2d)
+
+# Contact
+
+Please feel free to get in touch if you have questions, in particular about TDA and applications: [muncheli@msu.edu](mailto:muncheli@msu.edu)
