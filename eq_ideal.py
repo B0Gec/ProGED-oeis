e7f261439a00e8f0ccb570aa4a5305ca0e6a4c58
bogec <bostogec@gmail.com>
2024-10-17 17:00:18 +0200

    filtering MB output

--- eq_ideal.py
+++ eq_ideal.py
@@ -1,4 +1,6 @@
-def ideal_to_eqs(ideal: str, complexity: int = 10) -> list:
+import re
+
+def ideal_to_eqs(ideal: str, complexity: int = 10, verbosity=1) -> list:
     """
     Function to convert ideal to parsimony equations, which can be checked for correctness.
 
@@ -9,24 +11,103 @@ def ideal_to_eqs(ideal: str, complexity: int = 10) -> list:
     """
 
     generators_string = ideal[6:]
-    print('generators_string')
-    print(generators_string)
+    if verbosity > 0:
+        print('generators_string')
+        print(generators_string)
+        print(generators_string.replace(' ', ''))
+    # gens = generators_string.replace(' ', '').split(',')
     gens = generators_string.split(',')
+
     eqs = []
     for gen in gens:
-        summands = gen.split('+')
-        for sumand in summands:
-            print('   ', sumand)
-        print(len(summands))
+        if 'j' in gen:
+            raise ValueError('Repair split(\',  \'), since split from ideal to generator failed!!!.')
+        print(f'gen:{gen}')
+        summands = [i for i in gen.split(' ')]
+        summands = [j for j in [i.replace(' ', '') for i in summands] if j != '']  # clean-up empty strings.
+
+        if verbosity > 0:
+            for sumand in summands:
+                print('   ', sumand)
+            print(len(summands))
         if len(summands) <= complexity:
-            eqs += summands
+            eqs += [summands]
+
     print(gens)
+    eqs = sorted(eqs, key=lambda x: len(x))
 
     # eqs = gens
     for i, eq in enumerate(eqs):
-        print(f'eq {i}: {eq}')
+        print(f'len(eq {i}): {len(eq)}, eq {i}: {eq}')
 
     return eqs
+
+def bitsizef(equation: list[str]):
+    """Function determining the "bitsize" of an equation.
+        e.g. ['(1200/4345)*n*a(n-2)', 'a(n-1)'] \-> (4+4 + 0)/2 = 4
+
+    Args:
+        - eq (list): list of strings, representing the equation, result of ideal_to_eqs function.
+
+    Returns: int, representing the "bitsize" of the equation.
+        I.e. bitsize([coefficient*monomial for summand in equation]) := sum([bitsize(summand) for summand in equation])/(#summands),
+        where bitsize(coefficient) = (magnitude(nominator) + magnitude(denominator).
+
+    """
+
+    print('in bitsizef')
+
+    def bitsize_summand(summand: str):
+        """Returns the sum of number of digits of the numerator and denomumerator,
+        e.g. '+(-1200/4345)*n*a(n-2)(' \-> 4+4 = 8
+        """
+
+        print('in bitsizef_summand')
+        summand = ['a(n)', '-2*a(n-1)', 'n^4', '+(-7/16875)*a(n-1)^4', '-93090916800*n^2*a(n-1)', 'tm2*a' ][5]
+        print(summand)
+        coef = re.findall(r'^\+?\-?\(?\-?(\d+)/?(\d*)\)?', summand)
+        if coef == []:
+        #     # if re.findall(r'[a-zA-Z]', coef[0]) == []: return 0 else: raise ValueError('Variable have strange name or bug in code!!!.')
+            bits = 0
+        # elif:
+        else:
+            # return coef[0]
+            nom, denom = coef[0]
+            bits = len(nom)+len(denom)
+
+        print(coef)
+        print('return:', bits)
+        print('punchline')
+
+        1/0
+        coefficient, monomial = summand.split('*')
+
+        def bitsize_coefficient(coefficient: str):
+            """Function determining the "bitsize" of a coefficient.
+                e.g. '1200/4345' \-> (4+4)/2 = 4
+
+            Args:
+                - coefficient (str): string, representing the coefficient.
+
+            Returns: int, representing the "bitsize" of the coefficient.
+                I.e. bitsize(coefficient) := (magnitude(nominator) + magnitude(denominator))/2.
+
+            """
+
+            nominator, denominator = coefficient.split('/')
+            return (len(nominator) + len(denominator))
+
+        coefficient, monomial = summand.split('*')
+        return (bitsize_coefficient(coefficient) + len(monomial))/2
+
+
+    print(bitsize_summand(equation[1]))
+    1/0
+    bsize = sum([bitsize_summand(summand) for summand in equation])
+
+
+
+    return
 
 
 if __name__ == '__main__':
@@ -79,7 +160,12 @@ if __name__ == '__main__':
 
     print('po res')
     print(ideal)
-    eqs = ideal_to_eqs(ideal)
+    eqs = ideal_to_eqs(ideal, complexity=16)
+    print('eqs:', eqs)
+    bitsize = bitsizef(eqs[1])
+    print(bitsize)
+    1/0
+
     print('\n ------ after eqs ------')
     for eq in eqs:
         print(eq)
